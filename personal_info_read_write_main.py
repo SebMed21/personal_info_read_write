e5fa98fbf9e9a1fb513a970d31991c3e3714ae42
SebMed21 <sebastian.medina301426@gmail.com>
2024-11-28 14:15:57 +0800

    updated main, added looping

--- personal_info_read_write_main.py
+++ personal_info_read_write_main.py
@@ -3,21 +3,35 @@ last_name = []
 middle_name = []
 birthday = []
 
+retry_array = ["Y", "N"]
+retry_input = ""
 
-while True:
-    
-    user_input = str(input("Please enter your first name : "))
-    first_name.append(user_input)
-    
-    user_input = str(input("Please enter your last name : "))
-    last_name.append(user_input)
-    
-    user_input = str(input("Please enter your middle name : "))
-    middle_name.append(user_input)
-    
-    user_input = str(input("Please enter your birthday  : "))
-    birthday.append(user_input)
-    
+while True: 
+    while True:
+        try:
+            user_input = str(input("Please enter your first name : "))
+            first_name.append(user_input)
+            
+            user_input = str(input("Please enter your last name : "))
+            last_name.append(user_input)
+            
+            user_input = str(input("Please enter your middle name : "))
+            middle_name.append(user_input)
+                
+            user_input = str(input("Please enter your birthday  : "))
+            birthday.append(user_input)       
+            
+            retry_input = input("Would you like to try more? ( Y / N ) : ")
+            
+        except:
+            break
+        
+        while not retry_input in retry_array:
+            retry_input = input("Would you like to try more? ( Y / N ) : ")
+            
+        if retry_input in retry_array[1]: 
+                break
+            
     break
 
 with open("user_output.txt", "w") as output:
@@ -25,9 +39,3 @@ with open("user_output.txt", "w") as output:
     output.write(str(last_name))
     output.write(str(middle_name))
     output.write(str(birthday))
-
-print(f"{first_name}")
-print(f"{last_name}")
-print(f"{middle_name}")
-print(f"{birthday}")
-
