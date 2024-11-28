277cdc51f7319103ca60f50816e9de6211e3910a
SebMed21 <sebastian.medina301426@gmail.com>
2024-11-28 14:27:54 +0800

    added comments, made minor changes to main

--- personal_info_read_write_main.py
+++ personal_info_read_write_main.py
@@ -1,14 +1,15 @@
+#initialize all the arrays we'll be using
 first_name = []
 last_name = []
 middle_name = []
 birthday = []
-
+#i'm gonna use an array to store the correect answers to repeat the loop for convinience
 retry_array = ["Y", "N"]
-retry_input = ""
 
 while True: 
     while True:
         try:
+            #prompts the user for data
             user_input = str(input("Please enter your first name : "))
             first_name.append(user_input)
             
@@ -21,19 +22,24 @@ while True:
             user_input = str(input("Please enter your birthday  : "))
             birthday.append(user_input)       
             
+            #this asks the user if they want to repeat the loop
             retry_input = input("Would you like to try more? ( Y / N ) : ")
             
         except:
             break
         
+        
+        #this is what progresses the loop
         while not retry_input in retry_array:
             retry_input = input("Would you like to try more? ( Y / N ) : ")
             
+        #if the user answers "N" the loop will stop
         if retry_input in retry_array[1]: 
                 break
             
     break
 
+#this adds all the answers to the txt file
 with open("user_output.txt", "w") as output:
     output.write(str(first_name))
     output.write(str(last_name))
